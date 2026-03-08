d6225447003c96e15f1179a7634bca93ea9f4933
shuvo903 <saifulislamshuvoa123@gmail.com>
2026-03-09 01:10:33 +0600

    added Banner section and fixed Navbar some responsive issus

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,43 +1,49 @@
 import React from 'react';
 
 const Navbar = () => {
-    return (
-        <div>
-<div className="navbar bg-base-100 shadow-sm">
-  <div className="navbar-start">
-    <div className="dropdown">
-      <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
-        <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"> <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /> </svg>
-      </div>
-      <ul
-        tabIndex="-1"
-        className="menu menu-sm dropdown-content bg-base-100 rounded-box z-1 mt-3 w-52 p-2 shadow">
-        <li><a>Home</a></li>
-        <li><a>FAQ</a></li>
-        <li><a>Changelog</a></li>
-        <li><a>Blog</a></li>
-        <li><a>Download</a></li>
-        <li><a>Contact</a></li>
-      </ul>
-    </div>
-    <a className="text-2xl font-bold">CS — Ticket System</a>
-  </div>
-  <div className="navbar-end hidden lg:flex items-center text-lg font-medium">
-    <ul className="menu menu-horizontal px-1 items-center gap-4 ">
-        <li><a>Home</a></li>
-        <li><a>FAQ</a></li>
-        <li><a>Changelog</a></li>
-        <li><a>Blog</a></li>
-        <li><a>Download</a></li>
-        <li><a>Contact</a></li>
-    </ul>
-  <div className="flex items-center ml-4">
-    <a className="btn text-white font-semibold border-none bg- bg-linear-to-r from-[#632EE3] to-[#9F62F2] rounded-md">+ New Ticket</a>
-  </div>
-  </div>
-</div>
+  return (
+    <div  className='mx-auto max-w-screen-2xl px-3 lg:px-6'>
+      <div className="navbar bg-white">
+        <div className="navbar-start">
+          <a className="lg:text-2xl text-lg font-bold">CS — Ticket System</a>
         </div>
-    );
+
+        <div className="navbar-end">
+          <div className="dropdown dropdown-end lg:hidden">
+            <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
+              <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5 " fill="none" viewBox="0 0 24 24" stroke="currentColor"> <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /> </svg>
+            </div>
+            <ul
+              tabIndex={0}
+              className="menu menu-sm dropdown-content bg-base-100 rounded-box z-1 mt-3 w-52 p-2 shadow ">
+              <li><a>Home</a></li>
+              <li><a>FAQ</a></li>
+              <li><a>Changelog</a></li>
+              <li><a>Blog</a></li>
+              <li><a>Download</a></li>
+              <li><a>Contact</a></li>
+              <div className="flex items-center">
+                <a className="btn w-full text-white font-semibold border-none bg- bg-linear-to-r from-[#632EE3] to-[#9F62F2] rounded-md">+ New Ticket</a>
+              </div>
+            </ul>
+          </div>
+          <div className="hidden lg:flex items-center text-lg font-medium">
+            <ul className="menu menu-horizontal px-1 items-center gap-4  flex-nowrap">
+              <li><a>Home</a></li>
+              <li><a>FAQ</a></li>
+              <li><a>Changelog</a></li>
+              <li><a>Blog</a></li>
+              <li><a>Download</a></li>
+              <li><a>Contact</a></li>
+            </ul>
+            <div className="flex items-center ml-4 shrink-0 mr-4">
+              <a className="btn text-white font-semibold border-none bg- bg-linear-to-r from-[#632EE3] to-[#9F62F2] rounded-md">+ New Ticket</a>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Navbar;

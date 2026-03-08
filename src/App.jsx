103eb103ce18289b2ac08338b12a3edfb53edb8b
shuvo903 <saifulislamshuvoa123@gmail.com>
2026-03-09 01:55:47 +0600

    added my footer section full responsive for mobile and added all link also

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 
 import './App.css'
 import Banner from './components/Banner/Banner'
+import Footer from './components/Footer/Footer'
 import Navbar from './components/Navbar/Navbar'
 
 function App() {
@@ -12,6 +13,8 @@ function App() {
       <Navbar></Navbar>
 
       <Banner></Banner>
+
+      <Footer></Footer>
     </>
   )
 }
---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,56 @@
+import React from 'react';
+import facebookIcon from "../../assets/facebook.png";
+import linkdinIcon from "../../assets/linkdin.png";
+import xIcon from "../../assets/x.png";
+import mailIcon from "../../assets/mail.png";
+
+const Footer = () => {
+    return (
+        <div>
+            <footer className="footer sm:footer-horizontal bg-black  p-10 text-white">
+                <aside>
+                    <h2 className="lg:text-2xl text-lg font-bold">CS — Ticket System</h2>
+                    <p className='text-sm text-stone-400'>
+                        We are dedicated to providing seamless support <br /> solutions that  bridge the gap between businesses <br />  and their customers. Our platform ensures every <br /> query is tracked, managed, and resolved with the <br />  highest level of efficiency and care
+                    </p>
+                </aside>
+                <nav>
+                    <h6 className="footer-title opacity-100 font-semibold text-white">Company</h6>
+                    <a className="link link-hover text-stone-400">About Us</a>
+                    <a className="link link-hover text-stone-400">Our Mission</a>
+                    <a className="link link-hover text-stone-400">Contact Saled</a>
+                </nav>
+                <nav>
+                    <h6 className="footer-title opacity-100 font-semibold text-white">Services</h6>
+                    <a className="link link-hover text-stone-400">Products & Services</a>
+                    <a className="link link-hover text-stone-400">Customer Stories</a>
+                    <a className="link link-hover text-stone-400">Download Apps</a>
+
+                </nav>
+                <nav>
+                    <h6 className="footer-title opacity-100 font-semibold text-white">Information</h6>
+                    <a className="link link-hover text-stone-400">Privacy Policy</a>
+                    <a className="link link-hover text-stone-400">Terms & Conditions</a>
+                    <a className="link link-hover text-stone-400">Join Us</a>
+                </nav>
+                <nav>
+
+                    <h6 className="footer-title opacity-100 font-semibold text-white">Social Links</h6>
+                    <a target='_blank' href='https://x.com/' className="flex mb-1.5 gap-2 link link-hover text-stone-400"> <img src={xIcon} alt="" /> @CS — Ticket System</a>
+                    <a target='_blank' href='https://www.linkedin.com/' className="flex mb-1.5 gap-2 link link-hover text-stone-400"> <img src={linkdinIcon} alt="" /> @CS — Ticket System</a>
+                    <a target='_blank' href='https://www.facebook.com/' className="flex mb-1.5 gap-2 link link-hover text-stone-400"> <img src={facebookIcon} alt="" /> @CS — Ticket System</a>
+                    <a target='_blank' href='https://mail.google.com/mail/u/0/' className="flex gap-2 link link-hover text-stone-400"> <img src={mailIcon} alt="" /> support@cst.com</a>
+
+                </nav>
+
+            </footer>
+            <footer className="footer sm:footer-horizontal footer-center bg-black  p-4 text-white border-t-2 border-gray-500 ">
+                <aside>
+                    <p>Copyright © {new Date().getFullYear()} - © 2025 CS — Ticket System. All rights reserved.</p>
+                </aside>
+            </footer>
+        </div>
+    );
+};
+
+export default Footer;
